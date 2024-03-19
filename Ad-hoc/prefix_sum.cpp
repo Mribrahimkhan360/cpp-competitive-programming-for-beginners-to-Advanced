6701afb7171f1d7cd47fa8b84b208a3856caeb22
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2024-03-20 00:40:44 +0600

    Update prefix_sum.cpp

--- Ad-hoc/prefix_sum.cpp
+++ Ad-hoc/prefix_sum.cpp
@@ -2,37 +2,26 @@
 using namespace std;
 int main()
 {
-    int N;
+   int n;
+   cin>>n;
+   int arr[n+1];
+   for(int i=0;i<n;i++)cin>>arr[i];
 
-    cin>>N;
-    int arr[N+1];
+   int pre[n+1];
+   pre[0]=arr[0];
 
-    for(int i=0;i<N;i++)
-    {
-        cin>>arr[i];
-    }
-    int pre[N+1];
+   for(int i=1;i<n;i++)
+   {
+       pre[i]=pre[i-1]+arr[i];
+   }
 
-    pre[0] = arr[0];
-
-    for(int i=1;i<N;i++)
-    {
-        pre[i] = pre[i-1] + arr[i];
-
-    }
-
-    int Q;
-    cin>>Q;
-    while(Q--)
-    {
-        int l,r;
-        cin>>l>>r;
-        if(l==0)
-        {
-            cout<<pre[r]<<endl;
-        }
-        else{
-            cout<<pre[r] - arr[l-1]<<endl;
-        }
-    }
+   int q;
+   cin>>q;
+   while(q--)
+   {
+       int l,r;
+       cin>>l>>r;
+       if(l==0)cout<<pre[r]<<endl;
+       else cout<<pre[r]-pre[l-1]<<endl;
+   }
 }
