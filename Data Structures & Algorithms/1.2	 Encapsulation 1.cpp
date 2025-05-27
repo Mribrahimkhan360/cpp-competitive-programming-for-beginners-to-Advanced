f0d32cdebbd4b7dd78d1347bf943d18f15f010ee
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2025-05-27 21:50:55 +0600

    Update 1.2	 Encapsulation 1.cpp

--- Data Structures & Algorithms/1.2	 Encapsulation 1.cpp
+++ Data Structures & Algorithms/1.2	 Encapsulation 1.cpp
@@ -1,3 +1,11 @@
+
+/*
+ ভাবো, একটা ওষুধের ক্যাপসুলের মধ্যে ভিতরের উপাদানগুলো থাকে, কিন্তু আমরা শুধু বাইরের খোলস দেখি 
+ — ভিতরের জিনিসগুলোতে আমাদের হাত দেওয়া নিষেধ। ঠিক তেমনই!
+
+Think of it like a medicine capsule — the outer shell is visible, but the internal contents are hidden.
+This is similar to the concept of encapsulation in OOP, where internal data and logic are protected from external access.
+*/
 #include<bits/stdc++.h>
 using namespace std;
 class Student{
