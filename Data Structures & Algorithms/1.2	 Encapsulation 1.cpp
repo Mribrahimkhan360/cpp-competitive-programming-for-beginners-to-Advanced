de209f9a8760e2141cc57bc9bb638f2a33bd886f
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2025-05-27 21:42:10 +0600

    Create 1.2	 Encapsulation 1.cpp
    Object-Oriented Programming Using C++

---
+++ Data Structures & Algorithms/1.2	 Encapsulation 1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+private :
+    string name;
+public :
+    void setName(string x)
+    {
+        name=x;
+    }
+    string getName()
+    {
+        return name;
+    }
+};
+int main()
+{
+    Student s1;
+    s1.setName("hello");
+    cout<<s1.getName()<<endl;
+
+
+}
